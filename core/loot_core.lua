9eb4c55a762c980a0baf6381e2148af82b72e622
khlav <kirk.hlavka@gmail.com>
2025-11-03 17:20:04 -0500

    Remove showLootFrame() and rely on Blizzard default handlers
    - Removed showLootFrame() function from core/loot_core.lua
    - Removed GogoLoot.canOpenWindow flag and all references
    - Cleaned up all commented-out showLootFrame() calls in core/events.lua
    - Addon now relies entirely on Blizzard's default LOOT_OPENED handler
    - Fixes duplicate loot window issue

--- core/loot_core.lua
+++ core/loot_core.lua
@@ -5,21 +5,6 @@
 local ItemInfoCache = {}
 local ItemIDCache = {}
 
--- Flag to control whether loot window can be opened
-GogoLoot.canOpenWindow = false
-
--- Show the loot frame
-function GogoLoot:showLootFrame(reason, force)
-    if InCombatLockdown() then
-        GogoLoot._utils.debug("Tried to show loot frame while in combat! Blizzard restricted this.")
-        return
-    end
-
-    GogoLoot._utils.debug("Showing loot frame because ".. reason)
-    GogoLoot.canOpenWindow = false
-    LootFrame:GetScript("OnEvent")(LootFrame, "LOOT_OPENED")
-end
-
 -- Export caches for use by other modules
 GogoLoot._loot_core = {
     ItemInfoCache = ItemInfoCache,
